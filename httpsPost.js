3cdcf50d97f7e065c070e16e638b6c3e4224728a
Nur Bar <nbfbce@ymail.com>
2016-09-11 17:14:45 -0300

    added sender id

--- httpsPost.js
+++ httpsPost.js
@@ -1,38 +1,36 @@
 var https = require('https');
 
 var postOptions = {
-    host: 'onesignal.com',
-    method: 'POST',
-    path: '/api/v1/notifications',
-    headers: {
-        'Content-Type': 'application/json'
-    },
+	host: 'onesignal.com',
+	method: 'POST',
+	path: '/api/v1/notifications',
+	headers: {
+		'Content-Type': 'application/json'
+	},
 };
 
 exports.sendAlert = function (res) {
-    return https.request(postOptions, function (sendRes) {
+	return https.request(postOptions, function (sendRes) {
 
-        sendRes.setEncoding('utf8');
-        sendRes.on('data', function (chunk) {
-            console.log(chunk);
-        });
-        sendRes.on('end', function () {
-            console.log('No more data in response.');
-            res.send('Sent');
-        });
-        console.log(sendRes)
-    });
+		sendRes.setEncoding('utf8');
+		sendRes.on('data', function (chunk) {
+		});
+		sendRes.on('end', function () {
+			console.log('No more data in response.');
+			res.send('Sent');
+		});
+	});
 };
 
-exports.postCode = function (playerID,senderID) {
-    var postData = JSON.stringify({
-        'app_id': process.env.Onesignal_app_id,
-        'contents': {'en': 'Alert'},
-        'data': {'source': senderID},
-        'include_player_ids': [
-            playerID
-        ]
-    })
+exports.postCode = function (playerID, senderID) {
+	var postData = JSON.stringify({
+		'app_id': process.env.Onesignal_app_id,
+		'contents': {'en': 'Alert'},
+		'data': {'source': senderID},
+		'include_player_ids': [
+			playerID
+		]
+	})
 
-    return postData;
+	return postData;
 };
--- index.js
+++ index.js
@@ -9,44 +9,43 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
 const firebaseConfig = {
-    serviceAccount: {
-        type: process.env.SA_TYPE,
-        project_id: process.env.SA_PROJECT_ID,
-        private_key_id: process.env.SA_PRIVATE_KEY_ID,
-        private_key: process.env.SA_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        client_email: process.env.SA_CLIENT_EMAIL,
-        client_id: process.env.SA_CLIENT_ID,
-        auth_uri: "https://accounts.google.com/o/oauth2/auth",
-        token_uri: "https://accounts.google.com/o/oauth2/token",
-        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_x509_cert_url: process.env.SA_CLIENT_X509_CERT_URL
-    },
-    databaseURL: "https://redpager-e6f7c.firebaseio.com",
-    apiKey:process.env.Firebase_apiKey,
-    authDomain: "redpager-e6f7c.firebaseapp.com",
-    storageBucket: "redpager-e6f7c.appspot.com"
+	serviceAccount: {
+		type: process.env.SA_TYPE,
+		project_id: process.env.SA_PROJECT_ID,
+		private_key_id: process.env.SA_PRIVATE_KEY_ID,
+		private_key: process.env.SA_PRIVATE_KEY.replace(/\\n/g, '\n'),
+		client_email: process.env.SA_CLIENT_EMAIL,
+		client_id: process.env.SA_CLIENT_ID,
+		auth_uri: "https://accounts.google.com/o/oauth2/auth",
+		token_uri: "https://accounts.google.com/o/oauth2/token",
+		auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+		client_x509_cert_url: process.env.SA_CLIENT_X509_CERT_URL
+	},
+	databaseURL: "https://redpager-e6f7c.firebaseio.com",
+	apiKey: process.env.Firebase_apiKey,
+	authDomain: "redpager-e6f7c.firebaseapp.com",
+	storageBucket: "redpager-e6f7c.appspot.com"
 }
 
 var firebaseapp = firebase.initializeApp(firebaseConfig);
 
 app.post('/generateToken', function (req, res) {
-    var uid = req.body.uid;
-    var customToken = firebase.auth().createCustomToken(uid);
-    res.send(JSON.stringify({token: customToken}));
+	var uid = req.body.uid;
+	var customToken = firebase.auth().createCustomToken(uid);
+	res.send(JSON.stringify({token: customToken}));
 });
 
 app.post('/sendAlert', function (req, res) {
-    var targetPhone = req.body.target;
-    var sourcePhone = req.body.source;
-    console.log(req.body.target)
-    var db = firebaseapp.database().ref().child('users/' + targetPhone);
-    db.once("value", function (snapshot) {
-        var alert = httpsPost.sendAlert(res);
-        alert.write(httpsPost.postCode(snapshot.val(),sourcePhone));
-        alert.end();
-    })
+	var targetPhone = req.body.target;
+	var sourcePhone = req.body.source;
+	var db = firebaseapp.database().ref().child('users/' + targetPhone);
+	db.once("value", function (snapshot) {
+		var alert = httpsPost.sendAlert(res);
+		alert.write(httpsPost.postCode(snapshot.val(), sourcePhone));
+		alert.end();
+	})
 });
 
 app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
